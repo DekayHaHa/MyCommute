4821c42c6bd3f19be105f99939248cfc135d7f56
Devin Kapla <devinkapla@gmail.com>
2019-04-10 19:53:43 -0600

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  fetchData = () => {
+    this.props.getUser()
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.fetchData}>get faux data</button>
+      </div>
+    );
+  }
+}
+
+export default App;
